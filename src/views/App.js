5ae8a9d659f3edbe4406a038e5cfca5d3b277f45
Kamil <Kamil@>
2019-09-25 10:52:06 +0200

    add base url

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import history from 'config/history';
 
 import withAuth from 'hoc/withAuth';
@@ -9,12 +9,12 @@ import MainPage from './MainPage';
 
 function App() {
   return (
-    <Router history={history}>
-      <div className="App">
+    <Router history={history} basename={process.env.PUBLIC_URL}>
+      <Switch className="App">
         <Route path="/" exact component={withAuth(MainPage)} />
         <Route path="/login" component={AuthPage} />
         <Route path="/register" component={AuthPage} />
-      </div>
+      </Switch>
     </Router>
   );
 }
